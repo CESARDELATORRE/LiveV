c2ddbc47ec8514f55f7ad48c4488ac15a29b629c
Cesar De la Torre <cesardl@outlook.com>
2015-06-24 21:06:15 -0700

    New LiveVehicleActor

---
+++ LiveVehicleActor.Interfaces/ILiveVehicleActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+
+namespace LiveVehicleActor.Interfaces
+{
+    public interface ILiveVehicleActor : IActor
+    {
+        Task<int> GetCountAsync();
+
+        Task SetCountAsync(int count);
+    }
+}
---
+++ LiveVehicleActor/LiveVehicleActorState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using LiveVehicleActor.Interfaces;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Actors;
+
+namespace LiveVehicleActor
+{
+    [DataContract]
+    public class LiveVehicleActorState
+    {
+        [DataMember]
+        public int Count;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "LiveVehicleActorState[Count = {0}]", Count);
+        }
+    }
+}
